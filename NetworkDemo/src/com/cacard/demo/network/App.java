47178d3753e28118747d5a55177f4f6a1017e34b
CACARD <cacard@126.com>
2014-03-02 21:46:10 +0800

    network demo

---
+++ NetworkDemo/src/com/cacard/demo/network/App.java
@@ -0,0 +1,82 @@
+package com.cacard.demo.network;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class App {
+
+	public static void main(String[] args) {
+		testUrlUri();
+	}
+	
+	/**
+	 * URL URI
+	 */
+	public static void testUrlUri()
+	{
+		
+	}
+	
+	/**
+	 * NetworkInterface
+	 */
+	public static void testNetworkInterface()
+	{
+		try {
+			
+			NetworkInterface fa = NetworkInterface.getByName("eth0");
+			System.out.println(fa.getDisplayName());
+			
+			System.out.println("_______");
+			
+			Enumeration<NetworkInterface> faces = NetworkInterface.getNetworkInterfaces();
+			while(faces.hasMoreElements())
+			{
+				System.out.println(faces.nextElement().getDisplayName());
+			}
+			
+			
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * InetAddress
+	 */
+	public static void testInetAddress() {
+		try {
+			InetAddress[] add = InetAddress.getAllByName("localhost");
+
+			for (InetAddress a : add) {
+				System.out.println(a.getHostAddress());
+
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			InetAddress addr = InetAddress.getByName("www.diglog.com");
+
+			boolean b = addr.isReachable(5000);
+
+			System.out.println(b);
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
