ba0dcb6dc07cb12cc2c9353924dea2eeea04e2af
CACARD <cacard@126.com>
2014-03-05 18:06:51 +0800

    Socket and Channel and Selector

--- NetworkDemo/src/com/cacard/demo/network/AppSocket.java
+++ NetworkDemo/src/com/cacard/demo/network/AppSocket.java
@@ -1,12 +1,21 @@
+/**
+ * socket demo
+ * author:licunqing
+ */
+
 package com.cacard.demo.network;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class AppSocket {
 
+	private static int port=90;
+	
 	public static void main(String[] args)
 	{
 		startServerSocket();
@@ -21,33 +30,66 @@ public class AppSocket {
 		startClient();
 	}
 	
+	/**
+	 * Start Server to linstening
+	 */
 	public static void startServerSocket()
 	{
 		new Thread(new Runnable(){
-
 			@Override
 			public void run() {
+				
+				ServerSocket serverSocket=null;
 				try {
-					ServerSocket ss = new ServerSocket(90);
 					
+					serverSocket = new ServerSocket(port);
 					while(true)
 					{
-						System.out.println("accepting...");
-						ss.accept();
-						System.out.println("accepted...");
+						System.out.println("[server]server accepting...");
+						Socket clientSocket = serverSocket.accept();
+						System.out.println("[server]accepted");
+						
+						String result="";
+						int bufferLen=1024;
+						byte[] buffer = new byte[bufferLen];
+						InputStream is = clientSocket.getInputStream();
+						
+						int len=is.read(buffer, 0, bufferLen);
+						while(len>0)
+						{
+							result+=new String(buffer,"UTF-8").trim();
+							len=is.read(buffer, 0, bufferLen);
+						}
+						
+						System.out.println("->"+result);
+						
+						// write data
+						OutputStream os = clientSocket.getOutputStream();
+						os.write("hehe".getBytes("UTF-8"));
+						os.close();
+						
 					}
-					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				finally
+				{
+					try {
+						serverSocket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 				
 			}}).start();
-		
 	}
 	
+	/**
+	 * start client 
+	 */
 	public static void startClient()
 	{
-		for(int i=0;i<10;i++)
+		for(int i=0;i<1;i++)
 		{
 			System.out.println("thread "+i);
 			
@@ -55,14 +97,46 @@ public class AppSocket {
 
 				@Override
 				public void run() {
+					
+					Socket s = null;
 					try {
 						System.out.println("start a client socket:");
-						Socket s = new Socket("127.0.0.1",90);
+						s = new Socket("127.0.0.1",port);
+						s.setSoLinger(true, 0);
+						s.setTcpNoDelay(true);
 						
-						System.out.println("close a client socket");
-						s.close();
+						// TODO:如何实现Client端在写入数据之后，flush，然后read阻塞等待Server用这条连接返回的数据
+						
+						// write data
+						OutputStream stream = s.getOutputStream();
+						stream.write("Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.Hello.".getBytes("UTF-8"));
+						
+						stream.flush();
+						System.out.println("[client][after flush]socket is close?"+s.isClosed());
+						//stream.close();
+						//System.out.println("[client][after close]socket is close?"+s.isClosed());
+						
+						// read (blocking)
+						System.out.println("[client]try read");
+						InputStream is = s.getInputStream();
+						int len=is.read();
+						while(len>=0)
+						{
+							System.out.println("[client]reading...");
+							len=is.read();
+						}
+						
+						
 					} catch (Exception e) {
 						e.printStackTrace();
+					}
+					finally
+					{
+						try {
+							s.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 					
 				}}).start();
--- NetworkDemo/src/com/cacard/demo/network/AppSocketChannel.java
+++ NetworkDemo/src/com/cacard/demo/network/AppSocketChannel.java
@@ -1,24 +1,45 @@
 package com.cacard.demo.network;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class AppSocketChannel {
 
 	public static void main(String[] args)
 	{
+
+		// start server socket channel
+		new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				testServerSocketChannel();
+				
+			}}).start();
 		
-		//startServerSocket();
 		try {
 			Thread.currentThread().sleep(2000);
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		testSocketChannel();
+		
+		startClientConnectionInNewThread(90);
+		
+		//startClientConnectionInNewThread(91);
 		
 	}
 	
@@ -45,13 +66,154 @@ public class AppSocketChannel {
 	{
 		Selector selector = null;
 		
+		ServerSocketChannel channel1 = null;
+		ServerSocketChannel channel2 = null;
+		
 		try {
 			selector = Selector.open();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+		try {
+			channel1 = ServerSocketChannel.open();
+			channel1.configureBlocking(false);
+			ServerSocket ss1 = channel1.socket();
+			ss1.bind(new InetSocketAddress(90));
+			
+			channel2 = ServerSocketChannel.open();
+			ServerSocket ss2 = channel2.socket();
+			channel2.configureBlocking(false);
+			ss2.bind(new InetSocketAddress(91));
+			
+			// register
+			channel1.register(selector, SelectionKey.OP_ACCEPT);
+			channel2.register(selector, SelectionKey.OP_ACCEPT);
+			
+			while(true)
+			{
+				System.out.println("using select blocking ...");
+				selector.select(); // blocing until find one event
+				Iterator iter = selector.selectedKeys().iterator();
+					while(iter.hasNext())
+					{
+						SelectionKey key = (SelectionKey)iter.next();
+						iter.remove();
+						
+						if(key.isValid()==false)
+						{
+							continue;
+						}
+						
+						System.out.println(key.channel().toString());
+						if(key.isAcceptable())
+						{
+							// 变成了[connected local=/127.0.0.1:90 remote=/127.0.0.1:13441]
+							ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+							// 处理 accept
+							SocketChannel client = ssc.accept();
+							client.configureBlocking(false);
+							client.register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE); // Register Read
+							
+						}
+						else if(key.isReadable())
+						{
+							System.out.println("can read");
+							
+							SocketChannel client = (SocketChannel)key.channel(); // 这个时候key对应的channel就是SocketChannel
+							
+							ByteBuffer bf = ByteBuffer.allocate(1000);
+							int count = client.read(bf);
+							
+							System.out.println("read count="+count);
+							System.out.println("-->"+new String(bf.array(),"UTF-8"));
+							bf.flip();
+							
+							key.cancel();
+					
+							
+						}
+						else if(key.isWritable())
+						{
+							System.out.println("can write");
+						}
+
+					}
+
+				
+//				try {
+//					Thread.currentThread().sleep(3000);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+			}
+
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void startClientConnectionInNewThread(final int port)
+	{
+		new Thread(new Runnable(){
+
+			
+			@Override
+			public void run() {
+				
+	
+				Socket s=null;
+				
+				while(true){
+					
+
+					
+				try {
+					System.out.println("client socket try connect to port "+port);
+					s = new Socket("localhost",port);
+					OutputStream stream = s.getOutputStream();
+					String str="hello"+port;
+					stream.write(str.getBytes("UTF-8"));
+					stream.close();
+					
+					
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				finally
+				{
+					System.out.println("close client socket");
+					try {
+						s.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
+				try {
+					Thread.currentThread().sleep(16000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				break;
+				
+				}
+				
+				
 		
+				
+			}}).start();
+		
+		
+
 	}
 	
 	static void testSocketChannel()
