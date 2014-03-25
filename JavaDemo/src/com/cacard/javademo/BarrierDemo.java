10b3241ae478696587bb312b28e10a5cec7c40b1
CACARD <cacard@126.com>
2014-03-25 22:00:59 +0800

    fix

--- JavaDemo/src/com/cacard/javademo/BarrierDemo.java
+++ JavaDemo/src/com/cacard/javademo/BarrierDemo.java
@@ -4,28 +4,32 @@
  * 内存屏障指令是多个线程之间的数据同步。
  * 更多参考 MemoryBarrier/Barrier
  * 
- * Java下的CyclicBarrier意思是“周期性屏障”
+ * Java下的CyclicBarrier意思是“周期性/可复用屏障”
  */
-
 
 package com.cacard.javademo;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BarrierDemo {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		anotherDemo();
+	}
+
+	private void simpleDemo() {
 		final CyclicBarrier barrier = new CyclicBarrier(5);
-		
-		for(int i=0;i<10;i++)
-		{
-			new Thread(new Runnable(){
+
+		for (int i = 0; i < 10; i++) {
+			new Thread(new Runnable() {
 
 				@Override
 				public void run() {
-					System.out.println("thread"+Thread.currentThread().getId());
+					System.out.println("thread"
+							+ Thread.currentThread().getId());
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -33,17 +37,61 @@ public class BarrierDemo {
 					}
 					// ...
 					try {
-						System.out.println("await,ready count = "+barrier.await()); // 阻塞等待其它线程运行完毕
+						System.out.println("await,ready count = "
+								+ barrier.await()); // 阻塞等待其它线程运行完毕
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					} catch (BrokenBarrierException e) {
 						e.printStackTrace();
 					}
-					
-				}}).start();
+
+				}
+			}).start();
 		}
-		
-		System.out.println("集结结束,ready count is "+barrier.getNumberWaiting());
+
+		System.out.println("集结结束,ready count is " + barrier.getNumberWaiting());
 	}
-	
+
+	static volatile int loop = 1;
+
+	/**
+	 * CyclicBarrier(N,Runnable)
+	 */
+	private static void anotherDemo() {
+		final int threadCount = 5;
+		final int loopCount = 3;
+		final CyclicBarrier barrier = new CyclicBarrier(threadCount,
+				new Runnable() {
+
+					@Override
+					public void run() {
+						System.out.println("barrier is over and loop=" + loop);
+						loop++;
+					}
+				});
+
+		ExecutorService pool = Executors.newFixedThreadPool(threadCount);
+		for (int j = 0; j < loopCount; j++) {
+			for (int i = 0; i < threadCount; i++) {
+				pool.execute(new Runnable() {
+
+					@Override
+					public void run() {
+						System.out.println("thread"
+								+ Thread.currentThread().getId()
+								+ " running.already waiting count="
+								+ barrier.getNumberWaiting());
+						try {
+							barrier.await();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		}
+		pool.shutdown();
+
+	}
+
 }
