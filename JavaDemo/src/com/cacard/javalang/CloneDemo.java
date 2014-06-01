5f55abb5949767771f16c0c34a4f137e5d9d7397
CACARD <cacard@126.com>
2014-06-01 20:29:14 +0800

    clone

--- JavaDemo/src/com/cacard/javalang/CloneDemo.java
+++ JavaDemo/src/com/cacard/javalang/CloneDemo.java
@@ -4,9 +4,30 @@ import java.util.ArrayList;
 
 public class CloneDemo {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
+		ClassCanClone c = new ClassCanClone();
+		c.count=1;
 		
+		ClassCanClone cc = (ClassCanClone)c.clone();
+		
+		System.out.println(c==cc);
+		System.out.println(cc.count);
 	}
 	
+	public static class ClassCanClone implements Cloneable/*标志接口*/{
+		private int count;
+		
+		/**
+		 * clone()实现方式
+		 */
+		public ClassCanClone clone(){
+			ClassCanClone o=null;
+			try{
+				o=(ClassCanClone)super.clone();
+			}catch(CloneNotSupportedException e){
+				// pass
+			}
+			return o;
+		}
+	}
 }
